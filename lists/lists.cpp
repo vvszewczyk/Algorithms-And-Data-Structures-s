40ce6b45585f35b96ea91ed7d6f1aef25f85946b
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-04-13 16:15:47 +0200

    Add dropAtIndex func, move project to class

--- lists/lists.cpp
+++ lists/lists.cpp
@@ -1,176 +1,232 @@
 #include <iostream>
 
-class Node
+class LinkedList
 {
+    class Node
+    {
+      public:
+        int value;
+        Node *next;
+
+        Node(int val) : value(val), next(nullptr) {};
+    };
+
+    Node *head;
+
   public:
-    int value;
-    Node *next;
-
-    Node(int val) : value(val), next(nullptr) {};
-};
-
-void pushFront(Node *&head, int val)
-{
-    Node *newNode = new Node(val);
-
-    if (head == nullptr)
+    LinkedList() : head(nullptr) {};
+    ~LinkedList()
     {
-        head = newNode;
-        return;
-    }
-    else
-    {
-        newNode->next = head;
-        head = newNode;
-        return;
-    }
-}
-
-void pushBack(Node *&head, int val)
-{
-    Node *curr = head;
-
-    if (head == nullptr)
-    {
-        Node *newOne = new Node(val);
-        head = newOne;
-    }
-    else
-    {
+        Node *curr = head;
         while (curr)
         {
-            if (curr->next == nullptr)
-            {
-                Node *newOne = new Node(val);
-                curr->next = newOne;
-                break;
-            }
-
-            curr = curr->next;
+            Node *next = curr->next;
+            delete curr;
+            curr = next;
         }
-    }
-}
+    };
 
-void pushAtIndex(Node *&head, int data, std::size_t position)
-{
-    Node *curr = head;
-    Node *newNode = new Node(data);
+    void pushFront(int val)
+    {
+        Node *newNode = new Node(val);
 
-    if (head == nullptr)
-    {
-        head = newNode;
-        return;
-    }
-    else
-    {
-        if (position == 0)
+        if (head == nullptr)
         {
-            newNode->next = curr;
             head = newNode;
             return;
         }
-
-        std::size_t i = 0;
-        while (curr->next)
+        else
         {
-            if (i == position - 1)
-            {
-                Node *bufor = curr->next;
-                curr->next = newNode;
-                newNode->next = bufor;
-                return;
-            }
-            i++;
-            curr = curr->next;
+            newNode->next = head;
+            head = newNode;
+            return;
         }
-        // Walked through all list -> position is bigger than list size -> add on the end
-        curr->next = newNode;
     }
-}
 
-void dropFront(Node *&head)
-{
-    if (head == nullptr)
-    {
-        return;
-    }
-    else
-    {
-        Node *toDelete = head;
-        head = head->next;
-        delete toDelete;
-        return;
-    }
-}
-
-void dropBack(Node *&head)
-{
-    if (head == nullptr)
-    {
-        return;
-    }
-    else if (head->next == nullptr)
-    {
-        delete head;
-        head = nullptr;
-        return;
-    }
-    else
+    void pushBack(int val)
     {
         Node *curr = head;
 
-        while (curr)
+        if (head == nullptr)
         {
-            if (curr->next->next == nullptr)
+            Node *newOne = new Node(val);
+            head = newOne;
+        }
+        else
+        {
+            while (curr)
             {
-                Node *toDelete = curr->next;
-                curr->next = nullptr;
-                delete toDelete;
+                if (curr->next == nullptr)
+                {
+                    Node *newOne = new Node(val);
+                    curr->next = newOne;
+                    break;
+                }
+
+                curr = curr->next;
+            }
+        }
+    }
+
+    void pushAtIndex(int data, std::size_t position)
+    {
+        Node *curr = head;
+        Node *newNode = new Node(data);
+
+        if (head == nullptr)
+        {
+            head = newNode;
+            return;
+        }
+        else
+        {
+            if (position == 0)
+            {
+                newNode->next = curr;
+                head = newNode;
                 return;
             }
 
-            curr = curr->next;
+            std::size_t i = 0;
+            while (curr->next)
+            {
+                if (i == position - 1)
+                {
+                    Node *bufor = curr->next;
+                    curr->next = newNode;
+                    newNode->next = bufor;
+                    return;
+                }
+                i++;
+                curr = curr->next;
+            }
+            // Walked through all list -> position is bigger than list size -> add on the end
+            curr->next = newNode;
         }
     }
-}
 
-void dropAtIndex(Node *&head, std::size_t position)
-{
-    // TODO
-}
-
-void printList(Node *&head)
-{
-    Node *curr = head;
-    std::size_t index = 0;
-    while (curr)
+    void dropFront()
     {
-        std::cout << "i: " << index++ << " value: " << curr->value << "\n";
-        curr = curr->next;
+        if (head == nullptr)
+        {
+            return;
+        }
+        else
+        {
+            Node *toDelete = head;
+            head = head->next;
+            delete toDelete;
+            return;
+        }
     }
 
-    std::cout << "\n";
-}
+    void dropBack()
+    {
+        if (head == nullptr)
+        {
+            return;
+        }
+        else if (head->next == nullptr)
+        {
+            delete head;
+            head = nullptr;
+            return;
+        }
+        else
+        {
+            Node *curr = head;
+
+            while (curr)
+            {
+                if (curr->next->next == nullptr)
+                {
+                    Node *toDelete = curr->next;
+                    curr->next = nullptr;
+                    delete toDelete;
+                    return;
+                }
+
+                curr = curr->next;
+            }
+        }
+    }
+
+    void dropAtIndex(std::size_t position)
+    {
+        if (head == nullptr)
+        {
+            return;
+        }
+        else if (position == 0)
+        {
+            Node *toDelete = head;
+            head = head->next;
+            delete toDelete;
+        }
+        else
+        {
+            Node *curr = head;
+            std::size_t i = 0;
+            while (curr->next)
+            {
+                if (i == position - 1)
+                {
+                    Node *toDelete = curr->next;
+                    if (curr->next->next)
+                    {
+                        curr->next = curr->next->next;
+                    }
+                    else
+                    {
+                        curr->next = nullptr;
+                    }
+
+                    delete toDelete;
+                    return;
+                }
+
+                i++;
+                curr = curr->next;
+            }
+            return;
+        }
+    }
+
+    void printList() const
+    {
+        Node *curr = head;
+        std::size_t index = 0;
+        while (curr)
+        {
+            std::cout << "i: " << index++ << " value: " << curr->value << "\n";
+            curr = curr->next;
+        }
+
+        std::cout << "\n";
+    }
+};
 
 int main()
 {
-    Node *head = nullptr;
+    LinkedList list;
 
-    pushBack(head, 2);
-    pushBack(head, 3);
-    pushBack(head, 5);
+    list.pushBack(2);
+    list.pushBack(3);
+    list.pushBack(5);
 
-    pushAtIndex(head, 10, 3);
-    pushAtIndex(head, 11, 0);
-    pushAtIndex(head, 1000, 32131);
+    list.pushAtIndex(10, 3);
+    list.pushAtIndex(11, 0);
+    list.pushAtIndex(1000, 32131);
 
-    pushFront(head, 66);
-    pushFront(head, 21);
-    pushFront(head, 33);
+    list.pushFront(66);
+    list.pushFront(21);
+    list.pushFront(33);
 
-    dropFront(head);
-    dropBack(head);
-    dropBack(head);
+    list.dropFront();
+    list.dropBack();
+    list.dropBack();
 
-    printList(head);
+    list.dropAtIndex(2);
+
+    list.printList();
 }

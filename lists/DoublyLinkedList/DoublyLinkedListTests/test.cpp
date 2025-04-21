b949a80f9911a4b1f7e308fda40a1067fd8bf693
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-04-21 20:20:52 +0200

    Add tests for pushBack method

--- lists/DoublyLinkedList/DoublyLinkedListTests/test.cpp
+++ lists/DoublyLinkedList/DoublyLinkedListTests/test.cpp
@@ -35,3 +35,35 @@ TEST(DoublyLinkedListTests, PushFrontAddMultiple)
     EXPECT_EQ(head->next->next->prev->value, 20);
     EXPECT_EQ(head->next->next->next, nullptr);
 }
+
+TEST(DoublyLinkedListTests, PushBackWhenHeadEmpty)
+{
+    DoublyLinkedList list;
+    list.pushBack(15);
+    DoublyLinkedList::Node *head = list.getHead();
+
+    ASSERT_NE(head, nullptr);
+    EXPECT_EQ(head->value, 15);
+    EXPECT_EQ(head->next, nullptr);
+    EXPECT_EQ(head->prev, nullptr);
+}
+
+TEST(DoublyLinkedListTests, PushBackAddMultiple)
+{
+    DoublyLinkedList list;
+    list.pushBack(10);
+    list.pushBack(20);
+    list.pushBack(30);
+    DoublyLinkedList::Node *head = list.getHead();
+
+    ASSERT_NE(head, nullptr);
+    EXPECT_EQ(head->value, 10);
+    EXPECT_EQ(head->prev, nullptr);
+
+    EXPECT_EQ(head->next->value, 20);
+    EXPECT_EQ(head->next->prev->value, 10);
+
+    EXPECT_EQ(head->next->next->value, 30);
+    EXPECT_EQ(head->next->next->prev->value, 20);
+    EXPECT_EQ(head->next->next->next, nullptr);
+}

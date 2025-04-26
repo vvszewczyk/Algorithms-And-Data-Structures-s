4a7eca1a5e42b64749ae3c0712511f0d0cf06405
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-04-26 16:42:04 +0200

    Add first test for push method

---
+++ stack/Stack/StackTests/test.cpp
@@ -0,0 +1,24 @@
+// clang-format off
+#include "pch.h"
+// clang-format on
+#include "Stack.hpp"
+#include "gtest/gtest.h"
+
+TEST(StackTests, Push)
+{
+    Stack stack;
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+    Stack::Node *top = stack.getTop();
+    std::vector<int> expected = {30, 20, 10};
+
+    for (int &value : expected)
+    {
+        ASSERT_NE(top, nullptr);
+        EXPECT_EQ(top->value, value);
+        stack.pop();
+        top = stack.getTop();
+    }
+    EXPECT_EQ(top, nullptr);
+}

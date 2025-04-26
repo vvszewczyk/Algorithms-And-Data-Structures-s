90a1c8ab20dd8471e256b9594bf840fb4f62a7c6
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-04-26 19:50:12 +0200

    Add isEmpty peek size methods

--- stack/Stack/Stack/Stack.cpp
+++ stack/Stack/Stack/Stack.cpp
@@ -6,6 +6,12 @@ Stack::Stack() : top(nullptr) {};
 
 Stack::~Stack()
 {
+    while (top != nullptr)
+    {
+        Node *toDelete = top;
+        top = top->next;
+        delete toDelete;
+    }
 }
 
 Stack::Node *Stack::getTop()
@@ -31,10 +37,38 @@ void Stack::pop()
 {
     if (top == nullptr)
     {
-        return;
+        throw std::logic_error("Cannot pop empty stack");
     }
 
     Node *toPop = top;
     top = top->next;
     delete toPop;
+}
+
+bool Stack::isEmpty() const
+{
+    return top == nullptr;
+}
+
+int Stack::peek() const
+{
+    if (isEmpty())
+    {
+        throw std::out_of_range("Stack is empty");
+    }
+
+    return this->top->value;
+}
+
+std::size_t Stack::size() const
+{
+    std::size_t counter = 0;
+    Node *curr = top;
+    while (curr)
+    {
+        ++counter;
+        curr = curr->next;
+    }
+
+    return counter;
 }

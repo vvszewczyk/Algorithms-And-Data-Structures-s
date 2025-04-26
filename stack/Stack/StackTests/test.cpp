d726b7063bfe19cee76b29b78aff2d61f3182544
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-04-26 19:53:04 +0200

    Add test for pop method

--- stack/Stack/StackTests/test.cpp
+++ stack/Stack/StackTests/test.cpp
@@ -22,3 +22,27 @@ TEST(StackTests, Push)
     }
     EXPECT_EQ(top, nullptr);
 }
+
+TEST(StackTests, Pop)
+{
+    Stack stack;
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+    stack.push(40);
+    stack.push(10);
+
+    stack.pop();
+    stack.pop();
+    Stack::Node *top = stack.getTop();
+    std::vector<int> expected = {30, 20, 10};
+
+    for (int &value : expected)
+    {
+        ASSERT_NE(top, nullptr);
+        EXPECT_EQ(top->value, value);
+        stack.pop();
+        top = stack.getTop();
+    }
+    EXPECT_EQ(top, nullptr);
+}
